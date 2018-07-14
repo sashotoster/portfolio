16082c7c1ed170eece7adb4088827065f75ad1d4
sashotoster <sashotoster@gmail.com>
2018-07-14 23:12:32 +0200

    material-ui installation and test button

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="raised" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
