b99ff84058aaae844bc26581726168a9e3f78b12
Nikolay Vashchenko <sir.nickolas@gmail.com>
2018-07-23 20:22:05 +0200

    json helper

--- server.rb
+++ server.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 require 'sinatra/config_file'
+require 'sinatra/json'
 
 config_file 'config/config.yml'
-
 
 set :server, :puma
 set :port, 8080
 
 get '/' do
-  'Main api route'
+  json message: 'Main api route'
 end
