15d620470844d07036edbab8e69f251d6d56f723
Nikolay Vashchenko <sir.nickolas@gmail.com>
2018-07-25 20:51:00 +0200

    frontend compiling

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,11 +8,22 @@ set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :keep_releases, 2
 set :ssh_options, forward_agent: true
 set :format_options, log_file: 'shared/logs/capistrano.log'
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :npm_target_path, -> { release_path.join('client') } # default not set
-# set :bundle_jobs, 4 # Equals to amount of cores
+set :bundle_jobs, 2 # Equals to amount of cores
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # append :linked_files, "config/database.yml"
 append :linked_dirs, '.bundle'#, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
+after 'deploy:published', 'deploy:compile'
 after 'deploy:published', 'bundler:clean'
+
+
+namespace :deploy do
+  desc 'Build React frontend client'
+  task :compile do
+    on roles(:all) do
+      execute "cd #{release_path}/client; npm run-script build"
+    end
+  end
+end
