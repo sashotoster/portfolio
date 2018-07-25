17a430d320e5337f1e52a3c0ce16d78e93e3f351
Nikolay Vashchenko <sir.nickolas@gmail.com>
2018-07-25 21:23:51 +0200

    moving symlinked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,16 +7,16 @@ set :user, 'webmaster'
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :keep_releases, 2
 set :ssh_options, forward_agent: true
-set :format_options, log_file: 'shared/logs/capistrano.log'
+set :format_options, log_file: 'log/capistrano.log'
 set :npm_target_path, -> { release_path.join('client') } # default not set
 set :bundle_jobs, 2 # Equals to amount of cores
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # append :linked_files, "config/database.yml"
-append :linked_dirs, '.bundle'#, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, '.bundle', 'log', 'tmp'
 
 after 'deploy:published', 'deploy:compile'
-after 'deploy:published', 'bundler:clean'
+after 'deploy:published',  'bundler:clean'
 
 
 namespace :deploy do
@@ -26,4 +26,26 @@ namespace :deploy do
       execute "cd #{release_path}/client; npm run-script build"
     end
   end
+
+  desc 'Turn server on'
+  task :turn_on do
+    on roles(:all) do
+      execute "echo Not implemented"
+    end
+  end
+
+  desc 'Turn server off'
+  task :turn_ondo do
+    on roles(:all) do
+      execute "echo Not implemented"
+    end
+  end
+
+  desc 'Purge tmp'
+  task :purge_tmp do
+    on roles(:all) do
+      execute "echo Not implemented"
+    end
+  end
 end
+
--- config/puma.rb
+++ config/puma.rb
@@ -9,17 +9,16 @@ port 8080
 environment ENV['PORTFOLIO_ENV'] || 'production'
 
 app_dir = File.expand_path('../..', __FILE__)
-shared_dir = "#{app_dir}/shared"
 
 # Set up socket location
-#bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
