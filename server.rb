724273e9124c62f9979901ce8c9bea62eb699709
Nikolay Vashchenko <sir.nickolas@gmail.com>
2018-07-29 15:02:50 +0200

    configuring 404 route

--- server.rb
+++ server.rb
@@ -10,7 +10,12 @@ set :port, 8080
 set :environment, ENV['PORTFOLIO_ENV']
 
 namespace '/api' do
-  get '/' do
+  get '' do
     json message: 'Main API route'
   end
+end
+
+not_found do
+  status 404
+  json error: 'Page not found'
 end
