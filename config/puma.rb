b658a42eb915304650d7904f7a02bb0926e6e117
Nikolay Vashchenko <sir.nickolas@gmail.com>
2018-07-29 22:24:28 +0200

    preloading puma app

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,8 @@ workers 2
 # Min and Max threads per worker
 threads 1, 6
 
+preload_app!
+
 environment ENV['PORTFOLIO_ENV'] || 'production'
 
 app_dir = File.expand_path('../..', __FILE__)
@@ -14,11 +16,9 @@ else
   port 8080
 end
 
-activate_control_app
-
-on_worker_boot do
+#on_worker_boot do
   # Database connection
   # require "active_record"
   # ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   # ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-end
+#end
