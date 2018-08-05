cda7ddbb27c086df3faedbc23902fc800a92b597
Nikolay Vashchenko <sir.nickolas@gmail.com>
2018-08-05 13:53:38 +0200

    [back] rollbar configuration

--- server.rb
+++ server.rb
@@ -2,9 +2,17 @@ require 'sinatra'
 require 'sinatra/config_file'
 require 'sinatra/json'
 require 'sinatra/namespace'
+require 'rollbar/middleware/sinatra'
+
+Rollbar.configure do |config|
+  config.enabled = ENV['RACK_ENV'] == 'production'
+  config.environment = ENV['RACK_ENV']
+  config.access_token = ENV['BACKEND_ROLLBAR_TOKEN']
+end
 
 class Backend < Sinatra::Application
-  set :environment, (ENV['RACK_ENV'] || 'production')
+  use Rollbar::Middleware::Sinatra
+  set :environment, ENV['RACK_ENV']
 
   config_file 'config/config.yml'
 
