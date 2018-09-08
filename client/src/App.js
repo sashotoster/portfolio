dff8142a82d3af49d499630a71269eaddbfc9bbb
sashotoster <sashotoster@gmail.com>
2018-09-08 22:19:11 +0200

    [frnt] AboutMe about text is now an array of paragraphs

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,10 @@ function AboutMe (props) {
         </a>
     );
 
+    const aboutText = props.about.map((paragraph) =>
+        <p className="about-me-paragraph">{paragraph}</p>
+    );
+
     return (
         <div className="about-me">
             <Grid container spacing={24}>
@@ -25,7 +29,7 @@ function AboutMe (props) {
                     </Grid>
                 </Grid>
                 <Grid item container md={6} direction={"column"} justify={"space-between"}>
-                    <Grid item><div>{props.text}</div></Grid>
+                    <Grid item>{aboutText}</Grid>
                     <Grid item style={{marginTop: 10}}>{linkIcons}</Grid>
                 </Grid>
             </Grid>
@@ -42,7 +46,7 @@ class App extends Component {
     return (
         <div className="App">
             <NameTitle name="Alexandra Rys"/>
-            <AboutMe photo={this.state.photo} text={this.state.text} links={this.state.links}/>
+            <AboutMe photo={this.state.photo} about={this.state.about} links={this.state.links}/>
             <Navigation/>
             <Projects/>
         </div>
@@ -51,7 +55,11 @@ class App extends Component {
   getInitialState() {
       return {
           photo: 'https://cdn.cnn.com/cnnnext/dam/assets/141230145304-one-square-meter---logo-large-169.png',
-          text: 'here goes the text'.repeat(50),
+          about: [
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+              'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+              'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          ],
           links: [
               {icon: 'github', href: 'https://github.com/sashotoster'},
               {icon: 'instagram', href: 'https://www.instagram.com/sashotoster'},
