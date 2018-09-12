9dd0d44b37325ef73e7bb08fa3d78d148e02bcd0
Nikolay Vashchenko <nikolay@rys.me>
2018-09-12 08:11:03 +0200

    [frnt] enable compression in dev, because on prod it handled by the deployment script

--- client/config/webpackDevServer.config.js
+++ client/config/webpackDevServer.config.js
@@ -30,7 +30,7 @@ module.exports = function(proxy, allowedHost) {
     disableHostCheck:
       !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
     // Enable gzip compression of generated files.
-    compress: false,
+    compress: true,
     // Silence WebpackDevServer's own logs since they're generally not useful.
     // It will still show compile warnings and errors with this setting.
     clientLogLevel: 'none',
